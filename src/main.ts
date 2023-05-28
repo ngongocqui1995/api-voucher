7b53331699df0050437b281038b118cc69a9d353
qui <tranphanchau10@gmail.com>
2023-05-28 18:00:16 +0700

    commit vercel

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import * as path from 'path';
 import * as moduleAlias from 'module-alias';
 
 moduleAlias.addAliases({
-  '@src': path.resolve(__dirname, '..', 'src'),
+  '@src': path.resolve(__dirname),
 });
 
 async function bootstrap() {
